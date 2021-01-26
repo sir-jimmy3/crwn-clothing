505d57690018f1bf2a0a6b10721a5498c18d3817
sir-jimmy3 <blunt-94mail.ru>
2021-01-26 14:04:17 +0300

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,16 +3,40 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const config = {
-    apiKey: "AIzaSyDpp0JUIIn1bP2BpdaRVcASTB1vNNjZOpI",    
-    authDomain: "crwn-db-fd88c.firebaseapp.com",
-    projectId: "crwn-db-fd88c",
-    storageBucket: "crwn-db-fd88c.appspot.com",
-    messagingSenderId: "868123835575",
-    appId: "1:868123835575:web:b5a235450144fc8a81e132",
-    measurementId: "G-M81HXLHTZN"
-  };
+  apiKey: "AIzaSyA6TB4AYJBIxTHO0IL3h0gkFFlZ1zN1HEo",
+  authDomain: "crwn-db-c15b1.firebaseapp.com",
+  projectId: "crwn-db-c15b1",
+  storageBucket: "crwn-db-c15b1.appspot.com",
+  messagingSenderId: "688585757318",
+  appId: "1:688585757318:web:765cb2a5616c651d2ef707",
+  measurementId: "G-54KKMDYMRD"
+};
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error created a message', error.message);
+    }
+  }
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
--- src/pages/signin-and-signup/signin-and-signup.component.jsx
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import './signin-and-signup.styles.scss';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInAndSignUpPage = () => (
-    <div className='signid-and-signup'>
+    <div className='signin-and-signup'>
         <SignIn />
+        <SignUp />
     </div>
 )
 
